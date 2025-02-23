6dd099b3c14266cb473d40d286e53f331a23fd9c
piratecaribou <170640850+piratecaribou@users.noreply.github.com>
2025-02-23 22:31:46 +0000

    Case Create Minecraft Is Fully Functional - Although AI suggestions to be added via a button soon

--- handlers/create-minecraft.js
+++ handlers/create-minecraft.js
@@ -1,11 +1,12 @@
-const { CommandInteraction, Attachment, MessageFlags, EmbedBuilder } = require('discord.js');
+const { MessageFlags, EmbedBuilder } = require('discord.js');
 const path = require("path");
 const fetch = require('node-fetch');
 const authenticator = require('./authenticator.js');
-const { databaseHost, databaseName, databaseUsername, databasePassword, request } = require('../config.json');
+const { databaseHost, databaseName, databaseUsername, databasePassword, request, edit } = require('../config.json');
 const mysql = require("mysql2/promise");
 const mime = require("mime-types");
 const fs = require('fs');
+const format = require('date-format');
 
 module.exports = async (interaction) => {
     await interaction.deferReply({ flags: MessageFlags.Ephemeral });
@@ -14,6 +15,7 @@ module.exports = async (interaction) => {
 
     let unique = false;
     let caseID = ''
+    const timestamp = Math.floor(Date.now() / 1000)
 
     // Sender Data
 
@@ -26,6 +28,11 @@ module.exports = async (interaction) => {
     const reason = interaction.options.getString('reason');
     const evidenceContentType = mime.extension(interaction.options.getAttachment('evidence').contentType);
     const evidenceURL = interaction.options.getAttachment('evidence').url;
+
+    // Error Embed
+    const errorEmbed = new EmbedBuilder()
+        .setColor(0xB22222)
+        .setDescription("An error occurred during this process, please alert <@658043211591450667>.")
 
     // Authenticator
 
@@ -57,15 +64,50 @@ module.exports = async (interaction) => {
                 "SELECT caseID FROM cases WHERE caseID = '" + caseID + "';"
             );
             if (caseIDSearch.length === 0) {unique = true;}
-        } catch (err) {console.log(err);}
+        } catch (err) {
+            await interaction.editReply({ embeds: [errorEmbed], flags: MessageFlags.Ephemeral });
+            console.log(err);
+            return
+        }
     }
 
-    //Download Evidence
+    // Download Evidence
 
     const evidencePath = path.join('./evidence', caseID + '-1.' + evidenceContentType);
     fetch(evidenceURL).then(res => res.body.pipe(fs.createWriteStream(evidencePath)));
 
+    // Send To Mysql Database
+    const connection = await mysql.createConnection({
+        //config.json
+        host: databaseHost,
+        user: databaseUsername,
+        database: databaseName,
+        password: databasePassword
+    });
+    try {
+        await connection.query(
+            "INSERT INTO cases (caseID, platform, perpetrator, executor, reason, evidence, time) VALUES ('" + caseID + "', 'minecraft', '" + username + "', '" + senderUserID + "', '" + reason + "', '" + caseID + "-1." + evidenceContentType + "', '" + timestamp + "');"
+        );
+    } catch (err) {
+        await interaction.editReply({ embeds: [errorEmbed], flags: MessageFlags.Ephemeral });
+        console.log(err);
+        return
+    }
 
-    await interaction.editReply({ content: `case id: ${caseID}`, flags: MessageFlags.Ephemeral });
+    // Send Success Embed
+    const successEmbed = new EmbedBuilder()
+        .setColor(0x6A5ACD)
+        .setDescription("Successfully created a minecraft punishment case.")
+        .addFields(
+            { name: "**Punished Player**", value: "`" + username + "`", inline: true },
+            { name: "**Reason**", value: "`" + reason + "`", inline: true },
+            { name: "**Case ID**", value: "`" + caseID + "`", inline: true },
+            { name: "**Case ID**", value: "`🏹` <@" + senderUserID + ">\n" + "`🕰️` <t:" + timestamp + ":R>", inline: true },
+            { name: "", value: "If you made a mistake, you can edit the case using" + edit + ".", inline: true }
+        )
+    await interaction.editReply({ embeds: [successEmbed], flags: MessageFlags.Ephemeral });
 
+    // Create Log Entry
+    const logEntry = format('dd/MM/yyyy hh:mm:ss', new Date()) + " » " + senderUsername + " (" + senderUserID + ") created a minecraft punishment case: " + caseID + "\n"
+    fs.appendFile('./logs/cases.log', logEntry, {encoding:'utf8'}, function(err) { if(err) { console.log(err); }});
 };
