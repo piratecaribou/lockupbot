c869aa0c707974ca1178f2608a611c41dd69f12c
piratecaribou <170640850+piratecaribou@users.noreply.github.com>
2025-02-23 16:42:45 +0000

    start

---
+++ commands/utility/case.js
@@ -0,0 +1,52 @@
+const { SlashCommandBuilder } = require('discord.js');
+const createMinecraft = require('../../handlers/create-minecraft.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('case')
+        .setDescription('Case base command')
+        .addSubcommandGroup(subcommandGroup =>
+            subcommandGroup
+                .setName('create')
+                .setDescription('Create base command')
+                .addSubcommand(subcommand =>
+                    subcommand
+                        .setName('minecraft')
+                        .setDescription('Create a minecraft punishment case')
+                        .addStringOption(option =>
+                            option.setName('username')
+                                .setDescription('The username of the player being punished')
+                                .setMaxLength(20)
+                                .setRequired(true))
+                        .addStringOption(option =>
+                            option.setName('reason')
+                                .setDescription('The reason for the punishment')
+                                .setMaxLength(255)
+                                .setRequired(true))
+                        .addAttachmentOption( option=>
+                            option.setName('evidence')
+                                .setDescription('The evidence for the punishment')
+                                .setRequired(true)))
+                .addSubcommand(subcommand =>
+                    subcommand
+                        .setName('discord')
+                        .setDescription('Crease a discord punishment case')
+                        .addUserOption( option=>
+                            option.setName('user')
+                                .setDescription('The user being punished')
+                                .setRequired(true))
+                        .addStringOption(option =>
+                            option.setName('reason')
+                                .setDescription('The reason for the punishment')
+                                .setMaxLength(255)
+                                .setRequired(true))
+                        .addAttachmentOption( option=>
+                            option.setName('evidence')
+                                .setDescription('The evidence for the punishment')
+                                .setRequired(true)))),
+    async execute(interaction) {
+        if (interaction.options.getSubcommand() === 'minecraft') {
+            createMinecraft(interaction);
+        }
+    },
+};
---
+++ handlers/authenticator.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql2/promise')
+const { host, database, username, password } = require('../config.json');
+module.exports = {
+    role: async (senderUserID) => {
+
+        const connection = await mysql.createConnection({
+            //config.json
+            host: host,
+            user: username,
+            database: database,
+            password: password
+        });
+
+        try {
+            const [results] = await connection.query(
+                "SELECT role FROM users WHERE userID = " + senderUserID + ";"
+            );
+            if (results.length === 0) {
+                return 'null'; // No user found
+            }
+            if (results[0].role === 'user') {
+                return 'user';
+            } else if (results[0].role === 'admin') {
+                return 'admin';
+            } else {
+                return 'null'; // Something vry wrong had to happen to get to this point
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+};
